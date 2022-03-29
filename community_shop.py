d60ac92ca6bfa56dc57f995e9519ead1989e6d5f
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2022-03-29 17:35:34 +0200

    Classes
    Still working on classes...

--- community_shop.py
+++ community_shop.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
 import asyncio
 import data
+import os
 import text
 
 
 class Market:
-    def __init__(self):
+    def __init__(self) -> None:
         self.data = data.community_shop()
-        asyncio.run(self._update())
+        asyncio.run(self.__update())
 
     def __str__(self) -> str:
         return text.community_shop_string(data.community_shop(), '')
+
+    def __len__(self) -> int:
+        return len(self.data)
     
-    async def _update(self) -> None:
+    async def __update(self) -> None:
         while (True):
-            await asyncio.sleep(1)
             if (self.data != data.community_shop()):
                 data.encode_community_shop(self.data)
+            await asyncio.sleep(1)
 
 
 class MarketForUser:
@@ -29,12 +33,10 @@ class MarketForUser:
         return text.community_shop_string(self.market.data, self.game)
 
     def __call__(self) -> None:
-        self.mainloop()
+        self.__mainloop()
 
-    def __del__(self) -> None:
-        ...
-
-    def choice() -> bool:
+    def __choice() -> bool:
+        os.system('cls')
         print("1) Take me to my market")
         print("2) Take me to the common market")
         answer = input("> ")
@@ -42,31 +44,94 @@ class MarketForUser:
             answer = input("> ")
         return answer == '2' # 2 => True, 1 => False
 
-    def mainloop(self) -> None:
-        if (self.choice()):
+    def __mainloop(self) -> None:
+        if (self.__choice()):
             while True:
+                os.system('cls')
                 print(self, '\n')
-                # Do what is needed
         else:
-            ...
+            SalesStall(self.market, self.user, self.game)()
 
 
 class SalesStall:
-    def __init__(self, market: Market, user: str, game: str):
+    def __init__(self, market: Market, user: str, game: str) -> None:
         self.user = user
         self.game = game
         self.market = market
+        self.content = self.market.data[self.game]
+    
+    def __str__(self) -> str:
+        return f"""
+        Welcome to your sales stall!
+
+        You have {data.money(self.user, self.game)}$
+        You currently have {len(self.content["Offers"])} offers
+        You currently have {len(self.content["Requests"])} requests
+    """
 
     def __call__(self) -> None:
-        self.mainloop()
-    
-    def mainloop(self) -> None:
+        self.__mainloop()
+
+    def __choice(self) -> bool:
+        os.system('cls')
+        print(self)
+        print("1) Go to offers")
+        print("2) Go to requests")
+        answer = input("> ")
+        while (answer not in {'1', '2'}):
+            answer = input("> ")
+        return answer == '1' # 1 => True, 2 => False        
+
+    def __mainloop(self) -> None:
         while True:
+            os.system()
             print(self, '\n')
-            # Do what is needed
+            if (self.__choice()):
+                Offers(self, self.user, self.game)()
+
+
+class Offers:
+    def __init__(self, sales_stall: SalesStall, user: str, game: str) -> None:
+        self.user = user
+        self.game = game
+        self.sales_stall = sales_stall
+        self.list = self.sales_stall.content["Offers"]
+
+    def __str__(self) -> str:
+        output = f"Your offers: "
+        for x, i in enumerate(self.list):
+            output += f'{x+1}) {i["Quantity"]} {i["Item"]} for {i["Price"]}$ each'
+        return output
+
+    def __call__(self) -> None:
+        self.__mainloop()
+
+    def __choice(self) -> bool:
+        os.system('cls')
+        print(self)
+        print("1) Go to market")
+        print("2) Go to requests")
+        answer = input("> ")
+        while (answer not in {'1', '2'}):
+            answer = input("> ")
+        return answer == '1' # 1 => True, 2 => False
+
+    def __mainloop(self) -> None:
+        while True:
+            os.system('cls')
+            print(self, '\n')
+            if (self.__choice()):
+                Requests(self.user, self.game)()
+
+
+class Requests:
+    def __init__(self, user: str, game: str) -> None:
+        self.user = user
+        self.game = game
+        self.content = data.community_shop()[game]["Requests"]
 
 
 def main(user: str, game: str) -> None:
     m = Market()
     mfu = MarketForUser(m, user, game)
-    ss = SalesStall(m, user, game)
+    mfu()
